b3ba1b63835a7d33602c5b1384b1973a3c6ec81a
drozdzu88 <drozdzu88@gmail.com>
2021-04-17 20:58:42 +0200

    My ToDoApp in Django

---
+++ todolist/forms.py
@@ -0,0 +1,12 @@
+from django import forms 
+from django.forms import ModelForm 
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+	class Meta:
+		model = Task
+		fields = "__all__"
+
+	title = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control form-control-lg", "placeholder": "Dodaj zadanie..."}),)
+
+	completed = forms.CharField(required=False, widget=forms.widgets.CheckboxInput(attrs={"class": "form-check-input"}),)
